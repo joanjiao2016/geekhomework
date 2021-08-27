4a8256a2ecbde583ebbf68ae479af5b5f3c3cda2
joanjiao2016 <42462099+joanjiao2016@users.noreply.github.com>
2021-08-27 10:59:21 +0800

    Update Index.scala

--- 0815/Index.scala
+++ 0815/Index.scala
@@ -1,1 +1,21 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+object ReadDirectort {
+  def main(args: Array[String]): Unit = {
+
+val sparkconf = new SparkConf()
+  .setAppName("words")
+  .setMaster("local[1]")
+    val sc = new SparkContext(sparkconf)
+
+    val data = sc.wholeTextFiles("hdfs://cdh2:8020/user/geekjoan")
+    //使用分割"/''获取文件名
+    val r1 = data.flatMap { x =>
+      val doc = x._1.split("/").last
+      //先按行切分,在按列空格进行切分
+      x._2.split("\r\n").flatMap(_.split(" ").map { y => (y, doc)})}
+    .groupByKey.map{case(x,y)=>(x,y.toSet.mkString(","))}
+    r1.foreach(println)
+  }
+
 
